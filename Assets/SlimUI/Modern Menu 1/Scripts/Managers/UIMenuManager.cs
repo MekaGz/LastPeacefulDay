b4e90c75b25a4c6e72733e2bd9d20cd14e5061cb
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-07-09 01:38:32 -0500

    menos codigo

--- Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
@@ -7,79 +7,46 @@ using UnityEngine.SceneManagement;
 namespace SlimUI.ModernMenu{
 	public class UIMenuManager : MonoBehaviour {
 		private Animator CameraObject;
-
-		// campaign button sub menu
-        [Header("MENUS")]
-        [Tooltip("The Menu for when the MAIN menu buttons")]
         public GameObject mainMenu;
-        [Tooltip("THe first list of buttons")]
         public GameObject firstMenu;
-        [Tooltip("The Menu for when the PLAY button is clicked")]
         public GameObject playMenu;
-        [Tooltip("The Menu for when the EXIT button is clicked")]
         public GameObject exitMenu;
-        [Tooltip("Optional 4th Menu")]
         public GameObject extrasMenu;
 
         public enum Theme {custom1, custom2, custom3};
-        [Header("THEME SETTINGS")]
+
         public Theme theme;
         private int themeIndex;
 
 		private const string scene = "SampleScene";
         public ThemedUIData themeController;
-
-        [Header("PANELS")]
-        [Tooltip("The UI Panel parenting all sub menus")]
         public GameObject mainCanvas;
-        [Tooltip("The UI Panel that holds the CONTROLS window tab")]
         public GameObject PanelControls;
-        [Tooltip("The UI Panel that holds the VIDEO window tab")]
         public GameObject PanelVideo;
-        [Tooltip("The UI Panel that holds the GAME window tab")]
         public GameObject PanelGame;
-        [Tooltip("The UI Panel that holds the KEY BINDINGS window tab")]
         public GameObject PanelKeyBindings;
-        [Tooltip("The UI Sub-Panel under KEY BINDINGS for MOVEMENT")]
         public GameObject PanelMovement;
-        [Tooltip("The UI Sub-Panel under KEY BINDINGS for COMBAT")]
         public GameObject PanelCombat;
-        [Tooltip("The UI Sub-Panel under KEY BINDINGS for GENERAL")]
         public GameObject PanelGeneral;
         
 
         // highlights in settings screen
-        [Header("SETTINGS SCREEN")]
-        [Tooltip("Highlight Image for when GAME Tab is selected in Settings")]
         public GameObject lineGame;
-        [Tooltip("Highlight Image for when VIDEO Tab is selected in Settings")]
         public GameObject lineVideo;
-        [Tooltip("Highlight Image for when CONTROLS Tab is selected in Settings")]
         public GameObject lineControls;
-        [Tooltip("Highlight Image for when KEY BINDINGS Tab is selected in Settings")]
         public GameObject lineKeyBindings;
-        [Tooltip("Highlight Image for when MOVEMENT Sub-Tab is selected in KEY BINDINGS")]
         public GameObject lineMovement;
-        [Tooltip("Highlight Image for when COMBAT Sub-Tab is selected in KEY BINDINGS")]
         public GameObject lineCombat;
-        [Tooltip("Highlight Image for when GENERAL Sub-Tab is selected in KEY BINDINGS")]
         public GameObject lineGeneral;
 
-        [Header("LOADING SCREEN")]
-		[Tooltip("If this is true, the loaded scene won't load until receiving user input")]
 		public bool waitForInput = true;
         public GameObject loadingMenu;
-		[Tooltip("The loading bar Slider UI element in the Loading Screen")]
         public Slider loadingBar;
         public TMP_Text loadPromptText;
 		public KeyCode userPromptKey;
 
-		[Header("SFX")]
-        [Tooltip("The GameObject holding the Audio Source component for the HOVER SOUND")]
         public AudioSource hoverSound;
-        [Tooltip("The GameObject holding the Audio Source component for the AUDIO SLIDER")]
         public AudioSource sliderSound;
-        [Tooltip("The GameObject holding the Audio Source component for the SWOOSH SOUND when switching to the Settings Screen")]
         public AudioSource swooshSound;
 
 		void Start(){
@@ -124,13 +91,6 @@ namespace SlimUI.ModernMenu{
 			if(extrasMenu) extrasMenu.SetActive(false);
 			playMenu.SetActive(true);
 		}
-		
-		public void PlayCampaignMobile(){
-			exitMenu.SetActive(false);
-			if(extrasMenu) extrasMenu.SetActive(false);
-			playMenu.SetActive(true);
-			mainMenu.SetActive(false);
-		}
 
 		public void ReturnMenu(){
 			playMenu.SetActive(false);
@@ -141,13 +101,6 @@ namespace SlimUI.ModernMenu{
 
 		public void CargarDirecto(){
 			SceneManager.LoadScene(scene);
-		}
-		public void LoadScene(string scene)
-		{
-			if (scene != "")
-			{
-				StartCoroutine(LoadAsynchronously(scene));
-			}
 		}
 
 		public void  DisablePlayCampaign(){
@@ -239,26 +192,12 @@ namespace SlimUI.ModernMenu{
 		public void PlaySwoosh(){
 			swooshSound.Play();
 		}
-
-		// Are You Sure - Quit Panel Pop Up
 		public void AreYouSure(){
 			exitMenu.SetActive(true);
 			if(extrasMenu) extrasMenu.SetActive(false);
 			DisablePlayCampaign();
 		}
 
-		public void AreYouSureMobile(){
-			exitMenu.SetActive(true);
-			if(extrasMenu) extrasMenu.SetActive(false);
-			mainMenu.SetActive(false);
-			DisablePlayCampaign();
-		}
-
-		public void ExtrasMenu(){
-			playMenu.SetActive(false);
-			if(extrasMenu) extrasMenu.SetActive(true);
-			exitMenu.SetActive(false);
-		}
 
 		public void QuitGame(){
 			#if UNITY_EDITOR
@@ -268,30 +207,5 @@ namespace SlimUI.ModernMenu{
 			#endif
 		}
 
-		// Load Bar synching animation
-		IEnumerator LoadAsynchronously(string sceneName){ // scene name is just the name of the current scene being loaded
-			AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-			operation.allowSceneActivation = false;
-			mainCanvas.SetActive(false);
-			loadingMenu.SetActive(true);
-
-			while (!operation.isDone){
-				float progress = Mathf.Clamp01(operation.progress / .95f);
-				loadingBar.value = progress;
-
-				if (operation.progress >= 0.9f && waitForInput){
-					loadPromptText.text = "Press " + userPromptKey.ToString().ToUpper() + " to continue";
-					loadingBar.value = 1;
-
-					if (Input.GetKeyDown(userPromptKey)){
-						operation.allowSceneActivation = true;
-					}
-                }else if(operation.progress >= 0.9f && !waitForInput){
-					operation.allowSceneActivation = true;
-				}
-
-				yield return null;
-			}
-		}
 	}
 }
